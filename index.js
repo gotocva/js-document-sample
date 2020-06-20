1555c4de405db6eb152181f768cbf855564e9a59
Sivabharathy <gotocva@gmail.com>
2020-06-20 18:24:01 +0530

    sample js doc

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+/**
+ * Get all users
+ */
+function getUsers() {
+    return null;
+}
+
+/**
+ * api to return all users
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+function index(req,res) {
+    
+}
+app.get("/", index);
+
